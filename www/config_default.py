0370c37ba9dd5f205ae1dcbc25205121d6283fb9
Venidi <venidi@163.com>
2016-11-18 17:30:35 +0800

    Initial commit

---
+++ www/config_default.py
@@ -0,0 +1,16 @@
+#! /usr/bin python3
+# -*- coding: utf-8 -*-
+
+configs = {
+	'debug': True,
+	'db':{
+		'host': '127.0.0.1',
+		'port': 3306,
+		'user': 'root',
+		'password': 'root',
+		'db': 'pythonweb'
+	},
+	'session': {
+		'secret': 'Python'
+	}
+}
---
+++ www/config_override.py
@@ -0,0 +1,10 @@
+#! /usr/bin python3
+# -*- coding:utf-8 -*-
+"""
+部署时配置
+"""
+configs = {
+	'db':{
+		'host': '127.0.0.1'
+	}
+}
